8f85acc38a11d4765babf991e2f2bfe0a3357068
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2025-02-22 04:07:01 -0700

    update

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,7 +3,7 @@ import '../styles/App.css'
 export function App() {
   return (
     <div>
-      <h1>Rock, Paper, Scissors Spook</h1>
+      <h1>Rock, Paper, Scissors Spook Game</h1>
     </div>
   )
 }
