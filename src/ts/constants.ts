1541c73728f411dc6d30dda9124d26f7b0000f97
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2025-03-02 00:06:35 -0700

    add: constants

---
+++ src/ts/constants.ts
@@ -0,0 +1,22 @@
+export const tokens = {
+  "rock": {
+    "name": "rock",
+    "url": "images/icon-rock.svg"
+  },
+  "paper": {
+    "name": "paper",
+    "url": "images/icon-paper.svg"
+  },
+  "scissors": {
+    "name": "scissors",
+    "url": "images/icon-scissors.svg"
+  },
+  "lizard": {
+    "name": "lizard",
+    "url": "images/icon-lizard.svg"
+  },
+  "spock": {
+    "name": "spock",
+    "url": "images/icon-spock.svg"
+  }
+}
